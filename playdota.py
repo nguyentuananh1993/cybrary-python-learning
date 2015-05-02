871d5f94c602020850f6215c139045b5a94851f3
nguyentuananh1993 <nguyentuananh1993@gmail.com>
2015-05-02 10:49:44 +0700

    add parse JSON module :

--- playdota.py
+++ playdota.py
@@ -1,8 +1,40 @@
 import urllib2  
-
+import sqlite3 as lite
+import json
 proxy_support = urllib2.ProxyHandler({"http":"http://218.97.194.202:80"})  
 opener = urllib2.build_opener(proxy_support)  
-url='http://mazii.net/api/mazii/%E5%85%AC/10'  
+url='http://mazii.net/api/mazii/%E8%A1%8C/10'  
 page = opener.open(url)  
-contents=page.read()  
+contents=page.read()
 print contents  
+
+jjob = json.loads(contents)
+print "DETAIL\n"
+print jjob['results'][0]['detail']
+
+print "COMPDETAIL\n"
+for compdetail in jjob['results'][0]['compDetail']:
+	print compdetail['w'],
+	print compdetail['h'],
+
+print "EXAMPLES\n"
+for example in jjob['results'][0]['examples']:
+	print example['w'],
+	print example['p'],
+	print example['m'],
+	print example['h'],
+
+
+
+# try:
+# 	check = 0
+# 	con = lite.connect("development.sqlite3")
+# 	cur = con.cursor()
+# 	# cur.execute("UPDATE comments SET body=?,user_id=? WHERE question_id = ?",('anhnt is here','100','1'))
+# 	con.commit()
+# except lite.Error, e:
+# 	print "Error %s" % e.args[0]
+# 	sys.exit()
+# finally:
+# 	if con:
+# 		con.close()
--- sqlite.py
+++ sqlite.py
@@ -3,12 +3,13 @@ import unicodedata, codecs, urllib2
 import string
 from bs4 import BeautifulSoup
 
-def readSoup(url):
-	url = "http://" + url
-	request=urllib2.Request(url)
-	response=urllib2.urlopen(request)
-	soup=BeautifulSoup(response,'html.parser')
-	return soup
+def readSoup(word,proxy):
+	proxy_support = urllib2.ProxyHandler({"http":proxy})  
+	opener = urllib2.build_opener(proxy_support)  
+	url='http://mazii.net/api/mazii/'+word+'/10'  
+	page = opener.open(url)  
+	contents=page.read()  
+	return contents  
 
 def main():
 	database_name = "javn.db"#str(raw_input("File name: "))
@@ -23,22 +24,12 @@ def main():
 		data = cur.fetchall()
 		for row in data:
 			try:
-				proxy_support = urllib2.ProxyHandler({"http":"http://218.97.194.202:80"})  
-				opener = urllib2.build_opener(proxy_support)  
-				url='http://mazii.net/api/mazii/'+row[1].encode("utf-8")+'/10'  
-				page = opener.open(url)  
-				contents=page.read()  
-				print contents  
+				print readSoup(row[1].encode("utf-8"),"http://218.97.194.202:80")
 			except:
 				check = 1
 			if check ==1:
 				try:
-					proxy_support = urllib2.ProxyHandler({"http":"http://111.161.126.99:80"})  
-					opener = urllib2.build_opener(proxy_support)  
-					url='http://mazii.net/api/mazii/'+row[1].encode("utf-8")+'/10'  
-					page = opener.open(url)  
-					contents=page.read()  
-					print contents  
+					print readSoup(row[1].encode("utf-8"),"http://111.161.126.99:80")
 				except:
 					check = 0
 	except lite.Error, e:
