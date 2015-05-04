9191610b564c4cc7631748e58d633d8ed90beda5
nguyentuananh1993 <nguyentuananh1993@gmail.com>
2015-05-04 11:42:25 +0700

    add bug.log & resolve issue #1

--- sqlite.py
+++ sqlite.py
@@ -1,9 +1,10 @@
 import sqlite3 as lite
 import unicodedata, codecs, urllib2
 import string
-from bs4 import BeautifulSoup
 import json
-
+import sys
+import traceback
+import socket
 def readSoup(word,proxy):
 	proxy_support = urllib2.ProxyHandler({"http":proxy})  
 	opener = urllib2.build_opener(proxy_support)  
@@ -12,36 +13,48 @@ def readSoup(word,proxy):
 	contents=page.read()  
 	return contents  
 
+def getSource(word):
+	url='http://mazii.net/api/mazii/'+word+'/10'  
+	htmltext = urllib2.urlopen(url).read()
+	return htmltext
+
 
 def appendData(conn,curs, item, contents):
 	jjob = json.loads(contents)
 	# print "DETAIL\n"
-	detail =  '\"' + jjob['results'][0]['detail'] +'\"'
+	if jjob['results'][0]['detail'] != None:
+		detail =  '\"' + jjob['results'][0]['detail'] +'\"'
+	else:
+		detail = '\"\"'
 	# print detail
 
 	# print "COMPDETAIL\n"
 	compDetail = "["
-	for compdetail in jjob['results'][0]['compDetail']:
-		compDetail += '{\"w\":'
-		compDetail += compdetail['w']
-		compDetail += '\",\"h\":'
-		compDetail += compdetail['h']
-		compDetail += '\"}'
-	compDetail += ']'
+	if jjob['results'][0]['compDetail'] != None:
+		for compdetail in jjob['results'][0]['compDetail']:
+			compDetail += '{\"w\":\"'
+			compDetail += compdetail['w'] if compdetail['w'] else ' '
+			compDetail += '\",\"h\":\"'
+			compDetail += compdetail['h'] if compdetail['h'] else ' '
+			compDetail += '\"}'
+		compDetail += ']'
 	# print compDetail
 	# print "EXAMPLES\n"
 	Example = "["
-	for example in jjob['results'][0]['examples']:
-		Example += '{\"w\":'
-		Example += example['w']
-		Example += '\",\"p\":'
-		Example += example['p']
-		Example += '\",\"m\":'
-		Example += example['m']
-		Example += '\",\"h\":'
-		Example += example['h']
-		Example += '\"}'
-	Example += ']'
+	if jjob['results'][0]['examples'] != None:
+		for example in jjob['results'][0]['examples']:
+			Example += '{\"w\":\"'
+			print example['w']
+			Example += example['w'] if example['w'] else ' '
+
+			Example += '\",\"p\":\"'
+			Example += example['p'] if example['p'] else ' '
+			Example += '\",\"m\":\"'
+			Example += example['m'] if example['m'] else ' '
+			Example += '\",\"h\":\"'
+			Example += example['h'] if example['h'] else ' '
+			Example += '\"}'
+		Example += ']'
 	# print Example
 	curs.execute('UPDATE kanji SET detail= ?,compDetail= ?, examples = ? WHERE id=?',(detail,compDetail, Example,item,))
 	conn.commit()
@@ -50,10 +63,11 @@ def main():
 	database_name = "javn.db"#str(raw_input("File name: "))
 	table_name = "kanji" #str(raw_input("Enter table name: "))
 	rows = 1 #int(raw_input("Number of row: "))
-	
+	proxy = "http://218.97.194.202:80"
 	fro = int(raw_input("From id: "))
 	to = int(raw_input("To id: "))
-
+	ip = socket.gethostbyname('mazii.net')
+	print type(ip)
 	con = None
 	try:
 		check = 0
@@ -63,20 +77,31 @@ def main():
 		cur.execute('select * from '+table_name + ' where id >= '+str(fro)+ " and id < "+str(to))
 		data = cur.fetchall()
 		for row in data:
-			try:
-				contents = readSoup(row[1].encode("utf-8"),"http://218.97.194.202:80")
-				print contents
-				appendData(con,cur,row[0],contents)
-			except:
-				check = 1
-			if check ==1:
+			if check == 0:
 				try:
-					contents = readSoup(row[1].encode("utf-8"),"http://111.161.126.99:80")
+					contents = getSource(row[1].encode("utf-8"))
 					print contents
 					appendData(con,cur,row[0],contents)
 					count +=1
-					print count
+					print "Crawl data from http://mazii.net ("+ip+"): data_seq="+ str(count)+" kanji_id="+str(row[0])+" kanji_detail="+str(row[1].encode("utf-8"))+" proxy=None"
+				except KeyboardInterrupt:
+					sys.exit()
 				except:
+					print sys.exc_info()[0]
+					# traceback.print_exc(file=sys.stdout)
+					check = 1
+			if check ==1:
+				try:
+					contents = readSoup(row[1].encode("utf-8"),proxy)
+					print contents
+					appendData(con,cur,row[0],contents)
+					count +=1
+					print "Crawl data from http://mazii.net ("+ip+"): data_seq="+ str(count)+" kanji_id="+str(row[0])+" kanji_detail="+str(row[1].encode("utf-8"))+" proxy="+proxy
+				except KeyboardInterrupt:
+					sys.exit()
+				except:
+					print sys.exc_info()[0]
+					# traceback.print_exc(file=sys.stdout)
 					check = 0
 	except lite.Error, e:
 		print "Error %s" % e.args[0]
