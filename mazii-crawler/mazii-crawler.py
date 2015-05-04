74928fe9dc5a290a04857c69d3a85fd2d9263c17
nguyentuananh1993 <nguyentuananh1993@gmail.com>
2015-05-04 20:40:11 +0700

    done issue #4

--- mazii-crawler/mazii-crawler.py
+++ mazii-crawler/mazii-crawler.py
@@ -6,6 +6,36 @@ import sys
 import traceback
 import socket
 import timeit
+import random
+
+
+
+def switchProxy():
+	proxy = []
+	proxy.append("http://222.255.237.69:3128")
+	proxy.append("http://115.84.178.93:3128")
+	proxy.append("http://42.112.31.40:1234")
+	proxy.append("http://123.30.75.115:80")
+	proxy.append("http://118.70.13.161:443")
+	proxy.append("http://222.255.27.150:3128")
+	proxy.append("http://113.160.48.58:8080")
+	proxy.append("http://113.161.77.184:8080")
+	proxy_flag = 1
+	number = 0
+	while proxy_flag ==1:
+		if number>=len(proxy):
+			number = 0
+		try:
+			contents = readSoup("%E5%AD%97",proxy[number])
+			proxy_flag = 0
+			return proxy[number]
+		except KeyboardInterrupt:
+			sys.exit()
+		except:
+			print "This proxy "+proxy[number]+" is blocked. Switching to another proxy."
+			proxy_flag = 1
+			number+=1
+
 
 def readSoup(word,proxy):
 	proxy_support = urllib2.ProxyHandler({"http":proxy})  
@@ -28,7 +58,6 @@ def appendData(conn,curs, item, contents):
 		detail =  '\"' + jjob['results'][0]['detail'] +'\"'
 	else:
 		detail = '\"\"'
-	# print detail
 
 	# print "COMPDETAIL\n"
 	compDetail = "["
@@ -40,7 +69,7 @@ def appendData(conn,curs, item, contents):
 			compDetail += compdetail['h'] if compdetail['h'] else ' '
 			compDetail += '\"}'
 		compDetail += ']'
-	# print compDetail
+
 	# print "EXAMPLES\n"
 	Example = "["
 	if jjob['results'][0]['examples'] != None:
@@ -56,7 +85,7 @@ def appendData(conn,curs, item, contents):
 			Example += example['h'] if example['h'] else ' '
 			Example += '\"}'
 		Example += ']'
-	# print Example
+
 	curs.execute('UPDATE kanji SET detail= ?,compDetail= ?, examples = ? WHERE id=?',(detail,compDetail, Example,item,))
 	conn.commit()
 
@@ -64,11 +93,12 @@ def main():
 	database_name = "javn.db"#str(raw_input("File name: "))
 	table_name = "kanji" #str(raw_input("Enter table name: "))
 	rows = 1 #int(raw_input("Number of row: "))
-	proxy = "http://218.97.194.202:80"
+	proxy = ""
+	initial = 0
 	fro = int(raw_input("From id: "))
 	to = int(raw_input("To id: "))
 	ip = socket.gethostbyname('mazii.net')
-	# print type(ip)
+
 	con = None
 	try:
 		check = 0
@@ -78,27 +108,11 @@ def main():
 		cur.execute('select * from '+table_name + ' where id >= '+str(fro)+ " and id < "+str(to))
 		data = cur.fetchall()
 		for row in data:
-			if check == 0:
-				try:
-					start = timeit.default_timer()
-					contents = getSource(row[1].encode("utf-8"))
-					# print contents
-					appendData(con,cur,row[0],contents)
-					count +=1
-					stop = timeit.default_timer()
-					print "Crawl data from http://mazii.net ("+ip+"): data_seq="+ str(count)+" kanji_id="+str(row[0])+" kanji_detail="+str(row[1].encode("utf-8"))+" proxy=None ttl="+str(round(stop - start,4))+'s'
-				except KeyboardInterrupt:
-					sys.exit()
-				except urllib2.HTTPError, e:
-					print "This ip is blocked. Switching to proxy "+proxy
-					check = 1
-				except urllib2.URLError, e:
-					print "HTTP 404 File Not Found"
-				except:
-					print sys.exc_info()[0]
-					# traceback.print_exc(file=sys.stdout)
-					check = 1
-			if check ==1:
+			if initial == 0:
+				proxy = switchProxy()
+				initial = 1
+			check = 0
+			while(check!=1):
 				try:
 					start = timeit.default_timer()
 					contents = readSoup(row[1].encode("utf-8"),proxy)
@@ -107,17 +121,15 @@ def main():
 					count +=1
 					stop = timeit.default_timer()
 					print "Crawl data from http://mazii.net ("+ip+"): data_seq="+ str(count)+" kanji_id="+str(row[0])+" kanji_detail="+str(row[1].encode("utf-8"))+" proxy="+proxy+" ttl="+str(round(stop - start,4))+'s'
+					check = 1
 				except KeyboardInterrupt:
 					sys.exit()
 				except urllib2.HTTPError, e:
-					print "This proxy is blocked. Switching to main ip."
-					check = 0
+					proxy = switchProxy()
 				except urllib2.URLError, e:
 					print "HTTP 404 File Not Found"
 				except:
 					print sys.exc_info()[0]
-					# traceback.print_exc(file=sys.stdout)
-					check = 0
 	except lite.Error, e:
 		print "Error %s" % e.args[0]
 		sys.exit()
